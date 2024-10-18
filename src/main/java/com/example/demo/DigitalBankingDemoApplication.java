3c9925727a50d163a667ffd8c151bbf490bc4de1
Dimitri Tkavadze <dtkav22@freeuni.edu.ge>
2024-10-18 22:47:02 +0400

    initialized project

---
+++ src/main/java/com/example/demo/DigitalBankingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankingDemoApplication.class, args);
+	}
+
+}
