3671f41cf12247d6c5c4f6e20eb2ed820bd96468
Mate Zhorzholiani <zhorzholianimate@gmail.com>
2024-10-19 13:22:06 +0400

    remove test

--- src/test/java/BasicModelTests.java
+++ src/test/java/BasicModelTests.java
@@ -5,8 +5,5 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class BasicModelTests {
-    @Test
-    public void testUser() {
-        User user = new User("name1", "last name1", "1");
-    }
+
 }
