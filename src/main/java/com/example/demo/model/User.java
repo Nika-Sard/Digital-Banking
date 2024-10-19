a8c014980de352bda35a821e750f8db5c214e451
icosahedron30 <tarashdolaberidze8@gmail.com>
2024-10-19 11:41:01 +0400

    add user class

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,36 @@
 package com.example.demo.model;
 
+import java.util.ArrayList;
+
 public class User {
+    ArrayList<String> accounts;
+    private String firstName;
+    private String lastName;
+    String userId;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public ArrayList getAccounts() {
+        return accounts;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void addAccount(String accountId) {
+        accounts.add(accountId);
+    }
+    public void removeAccount(String accountId) {
+        accounts.remove(accountId);
+    }
+    User(String firstName, String lastName, String userId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userId = userId;
+    }
 }
